1fa8e902616f352a8ba59c893290d2ef069842a3
Gabriel Siu <gcsiu@edu.uwaterloo.ca>
2019-08-07 23:20:58 -0400

    Project initialization

---
+++ cookie-jar/cookie-jar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cookie-jar
+//
+//  Created by Gabriel Siu on 2019-08-07.
+//  Copyright © 2019 Gabriel Siu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
